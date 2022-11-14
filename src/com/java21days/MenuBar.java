0493cb6cf02dc65b25ce2ffee7bbc0d6fae2feba
Alejandro Salas <asalas.wd@outlook.com>
2022-11-14 11:49:30 -0500

    Create function to add buttons to array

--- src/com/java21days/MenuBar.java
+++ src/com/java21days/MenuBar.java
@@ -10,6 +10,7 @@ public class MenuBar extends JFrame {
         super(title);
         JPanel panel = new JPanel();
         setSize(dim);
+        JButton[] buttons = createButtons(options);
 
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -20,7 +21,12 @@ public class MenuBar extends JFrame {
 
     JButton[] createButtons(String[] options){
         JButton[] buttons = new JButton[options.length];
-
+        for(int i=0; i<options.length; i++){
+            ImageIcon icon = new ImageIcon(getClass().getResource("res/" + options[i].toLowerCase() + ".png"));
+            String text = options[i];
+            JButton btn = new JButton(text, icon);
+            buttons[i] = btn;
+        }
 
         return buttons;
     }
