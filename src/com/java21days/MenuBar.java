0b523ca531f8fd5a1feb7c9d12090374cb8baead
Alejandro Salas <asalas.wd@outlook.com>
2022-11-14 11:22:36 -0500

    Generate basic constructor

--- src/com/java21days/MenuBar.java
+++ src/com/java21days/MenuBar.java
@@ -1,4 +1,19 @@
 package com.java21days;
 
-public class MenuBar {
+import javax.swing.*;
+import java.awt.*;
+
+public class MenuBar extends JFrame {
+    Dimension dim = new Dimension(300, 300);
+
+    MenuBar(String title){
+        super(title);
+        JPanel panel = new JPanel();
+        setSize(dim);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+    }
 }
